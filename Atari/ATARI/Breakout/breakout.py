67de8063cba315cadd1389cba823b1bc68791177
Shanila Nehlin <nehlinshanila@gmail.com>
2024-05-17 01:01:39 +0600

    updated code and added report

--- Atari/ATARI/Breakout/breakout.py
+++ Atari/ATARI/Breakout/breakout.py
@@ -1,81 +1,39 @@
-import collections
-import cv2
 import gym
 import numpy as np
 import torch
 from PIL import Image
+import torchvision.transforms as T
 
 class DQNBreakout(gym.Wrapper):
-    def __init__(self, rendor_mode='rgb_array', repeat=4, device='cpu'):
-        env = gym.make("BreakoutNoFrameskip-v4", render_mode=rendor_mode)
-
+    def __init__(self, render_mode='rgb_array', device='cpu'):
+        env = gym.make("ALE/Breakout-v5", render_mode=render_mode)
         super(DQNBreakout, self).__init__(env)
-
-        self.image_shape = (84,84)
-        self.repeat = repeat
-        self.lives = env.ale.lives()
-        self.frame_buffer = []
         self.device = device
+        self.transform = T.Compose([
+            T.ToPILImage(),
+            T.Resize((84, 84)),
+            T.Grayscale(),
+            T.ToTensor(),
+            T.Normalize(mean=[0.5], std=[0.5])
+        ])
 
     def step(self, action):
-        total_reward = 0
-        done = False
-
-        for i in range(self.repeat):
-            observation, reward, done, truncated, info = self.env.step(action)
-
-            total_reward += reward
-
-            current_lives = info['lives']
-
-            if current_lives < self.lives:
-                total_reward = total_reward - 1
-                self.lives = current_lives
-
-                # print(f"Lives: {self.lives} total reward: {total_reward}")
-
-
-            self.frame_buffer.append(observation)
-
-            if done:
-                break
-
-        max_frame = np.max(self.frame_buffer[-2:], axis=0)
-        # max_frame = max_frame.to(self.device)
-        max_frame = self.process_observation(max_frame)
-        max_frame = max_frame.to(self.device)
-
-        total_reward = torch.tensor(total_reward).view(1, -1).float()
-        total_reward = total_reward.to(self.device)
-
-        done = torch.tensor(done).view(1, -1)
-        done = done.to(self.device)
-
-        return max_frame, total_reward, done, info
+        results = self.env.step(action)
+        observation = results[0]
+        reward = results[1]
+        done = results[2]
+        info = results[3] if len(results) > 3 else {}  # Handle cases where more than 4 values might be returned
+        processed_observation = self.process_observation(observation)
+        return processed_observation, reward, done, info
 
     def reset(self):
-        self.frame_buffer = []
-
-        observation, _ = self.env.reset()
-
-        self.lives = self.ale.lives()
-
-        observation = self.process_observation(observation)
-
-        return observation
-
+        observation = self.env.reset()
+        return self.process_observation(observation)
 
     def process_observation(self, observation):
-
-        img = Image.fromarray(observation)
-        img = img.resize(self.image_shape)
-        img = img.convert('L')
-        img = np.array(img)
-        img = torch.from_numpy(img)
-        img = img.unsqueeze(0)
-        img = img / 255.0
-
-        img = img.to(self.device)
-
-        return img
-
+        if isinstance(observation, tuple):
+            observation = observation[0]  # Handling tuple output from environment
+        if not isinstance(observation, (np.ndarray, torch.Tensor)):
+            raise TypeError(f"Expected observation to be np.ndarray or Tensor, got {type(observation)}")
+        observation = self.transform(observation).to(self.device)
+        return observation.unsqueeze(0)  # Add batch dimension
